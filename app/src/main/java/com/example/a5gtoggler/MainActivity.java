90a9dc4f5a2fb746b66aac2bcf1cf1f36e7d0e28
sanket <sanketyelugotla123@gmail.com>
2024-12-11 09:29:46 +0530

    Opening settings

--- app/src/main/java/com/example/a5gtoggler/MainActivity.java
+++ app/src/main/java/com/example/a5gtoggler/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,20 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Open mobile network settings directly
+        // Directly open mobile network settings
         Intent intent = new Intent(Settings.ACTION_NETWORK_OPERATOR_SETTINGS);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
+        startActivity(intent);
 
-            // Start the accessibility service to handle the button click
-            // Directly trigger the service as soon as settings page is opened
-            Intent accessibilityServiceIntent = new Intent(this, MyAccessibilityService.class);
-            startService(accessibilityServiceIntent);
-        } else {
-            System.err.println("Network settings activity not available!");
-        }
-
-        // Close the app immediately after opening the settings
+        // Close the app after launching the settings
         finish();
     }
 }
